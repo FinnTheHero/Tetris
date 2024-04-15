bb20fa677cd2b917c92481aa909e0f6588b8aee6
FinnTheHero <aleksandre.nozadze.1@iliauni.edu.ge>
2024-04-16 00:09:28 +0400

    ✨ Add LBlock class to handle L Tetris block and draw it.

--- src/grid.cc
+++ src/grid.cc
@@ -1,4 +1,5 @@
 #include "grid.h"
+#include "colors.h"
 
 Grid::Grid()
 {
@@ -37,18 +38,4 @@ void Grid::draw()
             DrawRectangle(startingX + (j * cellSize) + 2, startingY + (i * cellSize) + 2, cellSize - 2, cellSize - 2, colors[cellValue]);   
         }
     }
-}
-
-std::vector<Color> Grid::getColors()
-{
-    Color darkGrey = { 26, 31, 40, 255 };
-    Color green = { 47, 230, 23, 255 };
-    Color red = { 232, 18, 18, 255 };
-    Color orange = { 226, 116, 17, 255 };
-    Color yellow = { 237, 234, 4, 255 };
-    Color purple = { 166, 0, 247, 255 };
-    Color cyan = { 21, 204, 209, 255 };
-    Color blue = { 13, 64, 255, 255 };
-
-    return { darkGrey, green, red, orange, yellow, purple, cyan, blue };
 }
